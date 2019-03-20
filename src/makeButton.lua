f689e2a75974d52f7499330249de0fcfb5dcaa6c
Paul Doyle <pdoyle@roblox.com>
2019-03-20 14:04:12 -0700

    Changed button parameters! Updating the same branch, so that 'rotriever upgrade' will cause the lockfile to change if it pointed to this branch previously

--- src/makeButton.lua
+++ src/makeButton.lua
@@ -1,7 +1,7 @@
 local function makeButton()
 	local printButton = Instance.new("TextButton")
 	printButton.Text = "Click me!"
-	printButton.Size = UDim2.new(0, 200, 0, 200)
+	printButton.Size = UDim2.new(0, 200, 0, 100)
 	printButton.Position = UDim2.new(0.5, 0, 0.5, 0)
 	printButton.AnchorPoint = Vector2.new(0.5, 0.5)
 
